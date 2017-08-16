32971df0aae286b578a7b4ee87c5e0e16a67c48d
Chris Hansen <chris.hansen@modernmi.com>
2017-08-16 17:56:30 -0600

    uses "indexOf" to demonstrate matching word in string

--- annoy.js
+++ annoy.js
@@ -1,6 +1,14 @@
+// var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah") {
+// 	var answer = prompt("Are we there yet?");
+// }
+
+// alert("YAY, WE MADE IT!!!");
+
 var answer = prompt("Are we there yet?");
 
-while(answer !== "yes" && answer !== "yeah") {
+while(answer.indexOf("yes") === -1) {
 	var answer = prompt("Are we there yet?");
 }
 
