27fbd00acb980553ea9e96decafdea77394922fb
Chris Hansen <chris.hansen@modernmi.com>
2017-08-16 18:35:01 -0600

    final solution

--- forLoopProblemSet.js
+++ forLoopProblemSet.js
@@ -4,8 +4,10 @@ for(var i = -10; i <= 19; i++) {
 }
 
 console.log("Print all even numbers between 10 and 40");
-for(var i = 10; i <= 40; i+=2) {
-	console.log(i)
+for(var i = 10; i <= 40; i++) {
+	if(i % 2 === 0) {
+		console.log(i)
+	}
 }
 
 console.log("Print all odd numbers between 300 and 333");
