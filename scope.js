ebad883d12b53157f2d687f69f00cc356db5089a
Chris Hansen <chris.hansen@modernmi.com>
2017-08-18 14:43:43 -0600

    add more scope problems to existing files - final version

--- scope.js
+++ scope.js
@@ -19,3 +19,18 @@ function doSomething() {
 	var phrase = "Goodbye!";
 	console.log(phrase);
 }
+
+////////////
+
+function sing() {
+	console.log("twinkle twinkle...");
+	console.log("how i wonder...");
+}
+
+// setInterval(sing, 2000);
+// clearInterval(1);
+
+// setInterval(function(){
+// 	console.log("I am an anonymous function!");
+// 	console.log("THIS IS AWESOME!")
+// }, 2000);
