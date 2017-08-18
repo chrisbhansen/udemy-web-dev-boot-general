5bdbec11a4a424599e334ecd7764aad615298ac0
Chris Hansen <chris.hansen@modernmi.com>
2017-08-18 14:26:26 -0600

    simple exercise files for experimenting with scope - final version

---
+++ scope.js
@@ -0,0 +1,21 @@
+function doMath() {
+	var x = 40;
+	console.log(x);
+}
+var x = "hello";
+
+/////////////
+
+var y = 99;
+function doMoreMath() {
+	y = 100;
+	console.log(y);
+}
+
+////////////
+
+var phrase = "hi there!";
+function doSomething() {
+	var phrase = "Goodbye!";
+	console.log(phrase);
+}
