b42846f31a4245d6108049a580909f03b0af2789
Chris Hansen <chris.hansen@modernmi.com>
2017-08-19 15:43:12 -0600

    reverse array final

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -1,1 +1,34 @@
-alert("yep");
+var testArray = [1,2,3,4,5,6,7,8];
+var newArray = [];
+
+// printReverse()
+printArrayProblemTitle("Print Reverse");
+printStartingArray();
+printFinishedArray(printReverse());
+
+
+
+
+
+
+
+// business logic functions
+function printReverse() {
+	for(var i = testArray.length-1; i >= 0; i--) {
+		newArray.push(testArray[i]);
+	}
+	return newArray;
+}
+
+// display functions
+function printArrayProblemTitle(title) {
+	console.log(title);
+}
+
+function printStartingArray() {
+	console.log("Starting array: " + testArray)
+}
+
+function printFinishedArray(calledFunction) {
+	console.log("Finished array: " + calledFunction)
+}
