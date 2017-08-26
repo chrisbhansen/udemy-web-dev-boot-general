1ab09b2435f563d18e05d4f23ff676ccd2cc8662
Chris Hansen <chris.hansen@modernmi.com>
2017-08-26 17:53:11 -0600

    final version (before check against lesson)

--- arrayProblemSet.js
+++ arrayProblemSet.js
@@ -14,6 +14,20 @@ printStartingArray();
 printFinishedArray(isUniform());
 printDivider();
 
+// sumArray()
+testArray = [1,2,3,4,5,6,7,8];
+printArrayProblemTitle("Sum Array");
+printStartingArray();
+printFinishedArray(sumArray());
+printDivider();
+
+// max()
+testArray = [-1,-2,-3,-4,-5,-6,-7,-8];
+printArrayProblemTitle("Max Array");
+printStartingArray();
+printFinishedArray(max());
+printDivider();
+
 // business logic functions
 function printReverse() {
 	for(var i = testArray.length-1; i >= 0; i--) {
@@ -32,6 +46,26 @@ function isUniform() {
 		}
 	}
 	return true;
+}
+
+function sumArray() {
+	var sum = 0;
+	for(var i = 0; i < testArray.length; i++) {
+		sum = sum + testArray[i];
+	}
+	return sum;
+}
+
+function max() {
+	var max = Number.NEGATIVE_INFINITY;
+	for(var i = 0; i < testArray.length; i++) {
+		if (max <= testArray[i]) {
+			max = testArray[i];
+		} else {
+			// keep going - do nothing to "max"
+		}
+	}
+	return max;
 }
 
 // display functions
