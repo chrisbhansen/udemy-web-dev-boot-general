9cf78cf0ea33e571cbbb8b48ac0dc55037386c8f
Chris Hansen <chris.hansen@modernmi.com>
2017-08-27 15:21:48 -0600

    simple movie DB example (array of movie objects)

---
+++ movieDB.js
@@ -0,0 +1,29 @@
+var movies = [
+	{
+		title: "In Bruges",
+		rating: 5,
+		hasWatched: true
+	},
+	{
+		title: "Frozen",
+		rating: 3.5,
+		hasWatched: false
+	},
+	{
+		title: "Truman",
+		rating: 3,
+		hasWatched: true
+	}
+]
+
+movies.forEach(function(movie){
+	var result = "You have ";
+	if(movie.hasWatched){
+		result += "watched ";
+	} else {
+		result += "not seen ";
+	}
+	result += "\"" + movie.title + "\"";
+	result += " - " + movie.rating + " stars";
+	console.log(result);
+})
